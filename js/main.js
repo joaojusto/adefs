e1139b7015a5ab2c7d42650bc8f9e288e36937b1
João Justo <jpjustonunes@hotmail.com>
2015-05-04 11:08:17 +0100

    Fixes gallery

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/* jshint devel:true */
-"use strict";
-
-$("#gallery").justifiedGallery();
-$("#gallery_two").justifiedGallery();
-
-},{}]},{},[1]);
-
-//# sourceMappingURL=main.js.map
+!function r(e,t,i){function n(l,o){if(!t[l]){if(!e[l]){var f="function"==typeof require&&require;if(!o&&f)return f(l,!0);if(u)return u(l,!0);var a=new Error("Cannot find module '"+l+"'");throw a.code="MODULE_NOT_FOUND",a}var s=t[l]={exports:{}};e[l][0].call(s.exports,function(r){var t=e[l][1][r];return n(t?t:r)},s,s.exports,r,e,t,i)}return t[l].exports}for(var u="function"==typeof require&&require,l=0;l<i.length;l++)n(i[l]);return n}({1:[function(r,e,t){"use strict";$("#gallery").justifiedGallery(),$("#gallery_two").justifiedGallery()},{}]},{},[1]),$("#gallery").justifiedGallery(),$("#gallery_two").justifiedGallery();
